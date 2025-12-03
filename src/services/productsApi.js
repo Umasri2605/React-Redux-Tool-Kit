1d27ebb4c6f531c47af49ef2634fa77980a72451
Umasri537 <gubbalaumasri4@gmail.com>
2025-12-03 12:40:41 +0530

    RTK Components

--- src/services/productsApi.js
+++ src/services/productsApi.js
@@ -1,8 +1,6 @@
- // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
-// Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
   reducerPath: 'products Api',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://dummyjson.com/products'}),
@@ -13,6 +11,5 @@ export const productsApi = createApi({
   }),
 })
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
+
 export const {useGetAllproductsQuery} = productsApi
